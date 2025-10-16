f60f6c03c0c1b85dcca16ff8c39af50b22be9870
Mehdi <mahdykamali354@yahoo.com>
2025-10-16 11:41:57 +0330

    [enhancement] set default timezone to Asia/Tehran regardless of deployment

--- src/main/java/com/mahdy/cryptoarbitrage/CryptoArbitrageApp.java
+++ src/main/java/com/mahdy/cryptoarbitrage/CryptoArbitrageApp.java
@@ -6,6 +6,8 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 /**
  * @author Mehdi Kamali
  * @since 10/10/2025
@@ -17,6 +19,9 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class CryptoArbitrageApp {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Tehran"));
         SpringApplication.run(CryptoArbitrageApp.class, args);
     }
 }
+//TODO: uniform facade for calling exchange apis
+//TODO: support more coins? as limited config and enum values
